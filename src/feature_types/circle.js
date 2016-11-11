572ead0f873cc45be6daf130ba2332e95d12d858
Jonathan Huang <grokut@gmail.com>
2016-11-10 17:01:54 -0800

    add circle class

--- src/feature_types/circle.js
+++ src/feature_types/circle.js
@@ -1,35 +1,22 @@
-var Feature = require('./feature');
+var Polygon = require('./polygon');
 
 var Circle = function(ctx, geojson) {
-  Feature.call(this, ctx, geojson);
+  Polygon.call(this, ctx, geojson);
+  const toGeoJSON = this.toGeoJSON.bind(this)
+  this.toGeoJSON = function () {
+    const geoJSON = toGeoJSON()
+    return Object.assign({}, geoJSON, {
+      properties: Object.assign({}, this.properties, {
+        circle: true
+      }),
+      geometry: Object.assign({}, geoJSON.geometry, {
+        center: this.center,
+        radius: this.radius
+      })
+    })
+  }
 };
 
-Circle.prototype = Object.create(Feature.prototype);
-
-Circle.prototype.isValid = function() {
-  return this.coordinates.length > 1;
-};
-
-Circle.prototype.addCoordinate = function(path, lng, lat) {
-  this.changed();
-  var id = parseInt(path, 10);
-  this.coordinates.splice(id, 0, [lng, lat]);
-};
-
-Circle.prototype.getCoordinate = function(path) {
-  var id = parseInt(path, 10);
-  return JSON.parse(JSON.stringify(this.coordinates[id]));
-};
-
-Circle.prototype.removeCoordinate = function(path) {
-  this.changed();
-  this.coordinates.splice(parseInt(path, 10), 1);
-};
-
-Circle.prototype.updateCoordinate = function(path, lng, lat) {
-  var id = parseInt(path, 10);
-  this.coordinates[id] = [lng, lat];
-  this.changed();
-};
+Circle.prototype = Object.create(Polygon.prototype);
 
 module.exports = Circle;
